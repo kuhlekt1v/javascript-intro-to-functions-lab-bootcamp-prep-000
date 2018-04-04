5a71993d0db257e08da8815badc2766b32d528f0
Cody Sheridan <csheridan994@gmail.com>
2018-04-04 01:31:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function sayHiToGrandma(string) {
   if(string.toUpperCase() === string) {
     return ("YES INDEED!")  
   }
-  if (console.log("I love you, Grandma.") === string) {
+  if (string("I love you, Grandma.") === string) {
     return ('I love you, too.')
   }
 }
